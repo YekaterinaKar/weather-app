696211447185bbd4a9ee73fcf9f44f788388791d
André Silva <andre.silva@posteo.de>
2023-03-22 11:06:43 +0100

    Add form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,10 +1,19 @@
-import React, { PureComponent } from "react";
+import React from "react";
 
 export default function Form() {
   return (
     <form>
+      <h1>Add new Activity</h1>
+
+      <label htmlFor="">Name</label>
+      <br />
+      <input text="name" />
+      <br />
       <input type="checkbox" />
-      <label htmlFor="">Hello World</label>
+      <label>Good Weather Activity</label>
+      <br />
+      <button type="submit">Submit</button>
+      <br />
     </form>
   );
 }
