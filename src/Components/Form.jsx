b15fc83e5134e708813d31588f77c204927b4635
André Silva <andre.silva@posteo.de>
2023-03-22 10:53:01 +0100

    Add first rendering

---
+++ src/Components/Form.jsx
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+
+export default function Form() {
+  return (
+    <form>
+      <input type="checkbox" />
+      <label htmlFor="">Hello World</label>
+    </form>
+  );
+}
