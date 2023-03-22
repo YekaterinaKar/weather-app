67ca5fd8291f8c51bd727bfe400ab3bdaf080ac6
Katja <karnashauskaite@gmail.com~>
2023-03-22 11:20:14 +0100

    k

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+export function Form() {
+    <form>
+        <h1> Add new Activity </h1>
+        <label>Name</label>
+        <input name="name"></input>
+        <label> Check weather activity</label>
+        <input name="checkbox" type="checkbox"></input>
+        <button type="submit">Submit</button>
+    </form>;
+}
